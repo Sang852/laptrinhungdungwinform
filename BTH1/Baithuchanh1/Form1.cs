9d0293928dcdce7392b06b10c91a7f7f66b4fde3
Sang852 <sangd6216l@gmail.com>
2025-11-23 12:31:25 +0700

    Nộp bài 1 của bài thực hành 1

---
+++ BTH1/Baithuchanh1/Form1.cs
@@ -0,0 +1,37 @@
+﻿namespace Baithuchanh1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "Tên đăng nhập là: ";
+            thongbao += this.txtUser.Text;
+            thongbao += "\n\rMật khẩu là: ";
+            thongbao += this.txtPass.Text;
+            if (this.chkNho.Checked == true)
+            {
+                thongbao += "\n\rBạn có ghi nhớ.";
+            }
+            MessageBox.Show(thongbao, "Thông báo");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtUser.Clear();
+            this.txtPass.Clear();
+            this.chkNho.Checked = false;
+            this.txtUser.Focus();
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
